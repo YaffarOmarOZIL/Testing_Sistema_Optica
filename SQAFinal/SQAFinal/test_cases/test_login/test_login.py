1d41bc1b1340670deedc66e8d1f5fe3ddd782455
SaulCarpio <saulisraelcarpio90@gmail.com>
2024-11-25 15:11:51 -0400

    Hice el login xd

---
+++ SQAFinal/SQAFinal/test_cases/test_login/test_login.py
@@ -0,0 +1,29 @@
+# Incluir acá los test cases para el módulo específico.
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+ 
+class TestLogin:
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get("http://127.0.0.1:8000/")
+        self.driver.find_element(By.XPATH, "//a[text() = 'Iniciar Sesion']").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//input[@name='email']").send_keys("admin@gmail.com")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//input[@name='password']").send_keys("cinnamonroll")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
+        time.sleep(2)
+    
+    def teardown_method(self):
+        self.driver.quit()
+        print("Prueba visual completa")
+    
+    def test_verificar_login(self):
+        actual = self.driver.find_element(By.XPATH, "//h1[contains(@class, 'display-3')]").text
+        esperado = "¡Bienvenido a Optica Medop!"
+        assert esperado in actual, f"ERROR: Actual es: {actual} y el Esperado: {esperado}"
+
