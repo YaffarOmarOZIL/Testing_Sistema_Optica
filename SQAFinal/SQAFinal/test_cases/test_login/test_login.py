cab4a3b09e010b4556434e512e8567be37c2ea57
SaulCarpio <saulisraelcarpio90@gmail.com>
2024-11-25 15:47:08 -0400

    el log out ya esta xdd

--- SQAFinal/SQAFinal/test_cases/test_login/test_login.py
+++ SQAFinal/SQAFinal/test_cases/test_login/test_login.py
@@ -22,8 +22,17 @@ class TestLogin:
         self.driver.quit()
         print("Prueba visual completa")
     
-    def test_verificar_login(self):
+    def test_verificar_iniciar_sesion(self):
         actual = self.driver.find_element(By.XPATH, "//h1[contains(@class, 'display-3')]").text
         esperado = "¡Bienvenido a Optica Medop!"
+        assert esperado in actual, f"ERROR: Actual es: {actual} y el Esperado: {esperado}"
+
+    def test_vefiricar_cerrar_sesion(self):
+        self.driver.find_element(By.XPATH, "//a[@class='nav-link dropdown-toggle']").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//a[@class='dropdown-item text-danger']").click()
+        time.sleep(2)
+        actual = self.driver.find_element(By.XPATH, "//button[@type='submit']").text
+        esperado = "Iniciar sesión"
         assert esperado in actual, f"ERROR: Actual es: {actual} y el Esperado: {esperado}"
 
